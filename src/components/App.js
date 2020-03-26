00b17a9b462fa611b072ffe200fec5b3b9764423
snehalkurkute-cs <snehal.kurkute@contentserv.com>
2020-03-26 11:56:40 +0530

    StyleGuide: CSS changes
    Review: Vidit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
-import '../styleguide/_genericCSS.scss';
+import '../styleguide/app.scss';
 import React, { Component } from 'react';
 // import Button from './Button/Button';
 import { collapsibleVerticalMenu as VerticalMenu } from './vertical-menu/vertical-menu';
-import '../styleguide/_elements.scss';
 import MockDataForVerticalMenu from './vertical-menu/tack/mock-data-for-vertical-menu';
 
 class App extends Component {
